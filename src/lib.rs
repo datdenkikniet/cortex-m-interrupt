9e09c7746a49fd2993d1934125b88d2629c9b889
datdenkikniet <jcdra1@gmail.com>
2022-10-12 21:12:21 +0200

    Remove `ssq`

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,12 @@
 // Re-export this path
 pub use cortex_m;
 
-use core::task::Waker;
-
-mod ssq;
-use ssq::{Consumer, Producer, SingleSlotQueue};
-
 pub use cortex_m_interrupt_macro::take_raw_prio;
 
 #[cfg(feature = "rtic-priority")]
 pub use cortex_m_interrupt_macro::take;
 #[cfg(feature = "rtic-priority")]
 pub use rtic::export::logical2hw;
-
-pub type WakerQueue = SingleSlotQueue<Waker>;
-pub type WakerProducer<'a> = Producer<'a, Waker>;
-pub type WakerConsumer<'a> = Consumer<'a, Waker>;
 
 pub trait IrqHandle {
     fn register(self, f: fn());
