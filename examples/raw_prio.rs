9f36331d077c86b112ad664222bc6d1cf5998ebb
datdenkikniet <jcdra1@gmail.com>
2022-10-13 16:13:47 +0200

    Hack in the prio bits calculation

--- examples/raw_prio.rs
+++ examples/raw_prio.rs
@@ -1,10 +1,16 @@
 #![no_main]
 #![no_std]
 
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
 use cortex_m_interrupt::IrqHandle;
 
 fn _test() {
-    let handle_raw = cortex_m_interrupt::take_raw_prio!(stm32f1xx_hal::pac, EXTI9_5, 0b11110000);
+    let handle_raw =
+        cortex_m_interrupt::take_raw_prio!(stm32f1xx_hal::pac::interrupt::EXTI9_5, 0b11110000);
 
     handle_raw.register(|| panic!("Oo, raw priorities!"));
 }
