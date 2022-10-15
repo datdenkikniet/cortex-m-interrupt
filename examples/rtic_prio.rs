43c2fc78d1942cdca2eb046784474a13ec6755df
datdenkikniet <jcdra1@gmail.com>
2022-10-15 20:18:09 +0200

    Add kinder message about Exceptions (that are not NVIC-servicable)

--- examples/rtic_prio.rs
+++ examples/rtic_prio.rs
@@ -8,12 +8,10 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 
 use cortex_m_interrupt::IrqHandle;
 
-use cortex_m::peripheral::scb::Exception::SysTick;
 use stm32f1xx_hal::pac::interrupt::EXTI15_10;
 
 fn _test() {
     let handle = cortex_m_interrupt::take!(EXTI15_10, 4);
-    let handle = cortex_m_interrupt::take!(SysTick, 4);
 
     handle.register(|| panic!("Yhellow"));
 }
