e7a15ef5d232d5ad4e6c2e442dac6493fb856bb3
datdenkikniet <jcdra1@gmail.com>
2022-10-15 18:38:08 +0200

    Remove RTIC dependency and just import its `logical2hw` function
    Clean up dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,7 @@
 pub use cortex_m;
 use cortex_m::{interrupt::InterruptNumber, peripheral::NVIC};
 
-pub use cortex_m_interrupt_macro::take_raw_prio;
-
-#[cfg(feature = "rtic-priority")]
-pub use cortex_m_interrupt_macro::take;
-#[cfg(feature = "rtic-priority")]
-pub use rtic::export::logical2hw;
+pub use cortex_m_interrupt_macro::{take, take_raw_prio};
 
 /// A handle that can be used to register a handler for an interrupt.
 ///
@@ -62,4 +57,16 @@ pub unsafe fn determine_prio_bits(nvic: &mut NVIC, placeholder_interrupt: u16) -
     let prio_bits = written_prio.leading_ones();
 
     written_prio >> (8 - prio_bits)
+}
+
+/// Convert a logical priority (where higher priority = higher priority level) to
+/// a hardware priority level (where lower priority = higher priority level).
+///
+/// Taken from [`cortex_m_rtic`]
+///
+/// [`cortex_m_rtic`]: https://crates.io/crates/cortex-m-rtic
+#[inline]
+#[must_use]
+pub fn logical2hw(logical: u8, nvic_prio_bits: u8) -> u8 {
+    ((1 << nvic_prio_bits) - logical) << (8 - nvic_prio_bits)
 }
