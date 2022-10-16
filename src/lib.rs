8299e08239cca9d13516bb3e59790efe79d23075
datdenkikniet <jcdra1@gmail.com>
2022-10-16 16:22:36 +0200

    Add and use doc-cfg

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "unstable-doc-cfg", feature(doc_cfg))]
 #![no_std]
 
 // Re-exports
@@ -6,10 +7,12 @@ pub use cortex_m_rt::DefaultHandler_;
 
 pub use cortex_m_interrupt_macro::take;
 
-#[cfg(feature = "unstable")]
+#[doc_cfg::doc_cfg(feature = "unstable")]
 pub use cortex_m_interrupt_macro::take_nvic_interrupt;
 
+#[cfg(feature = "unstable")]
 mod nvic;
+#[cfg(feature = "unstable")]
 pub use nvic::*;
 
 /// A handle that can be used to register a handler for an interrupt.
